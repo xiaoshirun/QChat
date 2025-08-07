62d486d408af204653f2612b61b92477b668fa6d
QiuQiu <3113848820@qq.com>
2025-08-07 08:50:14 +0800

    gaibaian

---
+++ src/main/java/com/xsr/qchat/QChatApplication.java
@@ -0,0 +1,14 @@
+package com.xsr.qchat;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xsr/qchat/QChatApplicationTests.java
@@ -0,0 +1,17 @@
+package com.xsr.qchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootTest
+class QChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPassword = encoder.encode("123456");
+        System.out.println(encodedPassword);
+    }
+
+}
